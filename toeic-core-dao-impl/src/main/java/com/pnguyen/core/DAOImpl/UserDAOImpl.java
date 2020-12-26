bf70828bddd844425706e500cade6391f39f97d4
Nguyen Linh Phuong <nlphuong95@gmail.com>
2020-12-26 15:50:39 +0700

    Revert "Revert "Ticket #14 test API login""
    This reverts commit b8c5c2d3a4d3708c3ef55234ba4be775890c9c03.

--- toeic-core-dao-impl/src/main/java/com/pnguyen/core/DAOImpl/UserDAOImpl.java
+++ toeic-core-dao-impl/src/main/java/com/pnguyen/core/DAOImpl/UserDAOImpl.java
@@ -1,9 +1,68 @@
 package com.pnguyen.core.DAOImpl;
 
 import com.pnguyen.core.DAO.UserDAO;
+import com.pnguyen.core.common.utils.HibernateUtil;
 import com.pnguyen.core.data.DAOImpl.GenericDAOImpl;
 import com.pnguyen.core.persistence.entity.UserEntity;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDAOImpl extends GenericDAOImpl<Integer, UserEntity> implements UserDAO {
 
+    @Override
+    public UserEntity isUserExist(String name, String password) {
+        UserEntity entity = new UserEntity();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        try{
+            /*
+             * HQL
+             * */
+            StringBuilder sql = new StringBuilder();
+            sql.append("FROM UserEntity WHERE name= :name AND password= :password");
+            Query query = session.createQuery(sql.toString());
+            query.setParameter("name",name);
+            query.setParameter("password", password);
+            entity = (UserEntity) query.uniqueResult();
+            transaction.commit();
+        }catch (HibernateException e){
+            transaction.rollback();
+            throw e;
+        }finally {
+            session.close();
+        }
+        return entity;
+    }
+
+    @Override
+    public UserEntity findRoleByUser(String name, String password) {
+        UserEntity entity = new UserEntity();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        try{
+            /*
+             * HQL
+             * */
+            StringBuilder sql = new StringBuilder();
+            sql.append("FROM UserEntity WHERE name= :name AND password= :password");
+            Query query = session.createQuery(sql.toString());
+            query.setParameter("name",name);
+            query.setParameter("password", password);
+            entity = (UserEntity) query.uniqueResult();
+            transaction.commit();
+        }catch (HibernateException e){
+            transaction.rollback();
+            throw e;
+        }finally {
+            session.close();
+        }
+        return entity;
+    }
 }
