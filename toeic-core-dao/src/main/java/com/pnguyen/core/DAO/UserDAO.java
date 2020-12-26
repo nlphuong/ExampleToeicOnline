b8c5c2d3a4d3708c3ef55234ba4be775890c9c03
Nguyen Linh Phuong <nlphuong95@gmail.com>
2020-12-26 15:48:13 +0700

    Revert "Ticket #14 test API login"
    This reverts commit abc04213374b7cd77cbd04c7961b554092290fca.

--- toeic-core-dao/src/main/java/com/pnguyen/core/DAO/UserDAO.java
+++ toeic-core-dao/src/main/java/com/pnguyen/core/DAO/UserDAO.java
@@ -4,8 +4,5 @@ import com.pnguyen.core.data.DAO.GenericDAO;
 import com.pnguyen.core.persistence.entity.UserEntity;
 
 public interface UserDAO extends GenericDAO<Integer, UserEntity> {
-    UserEntity isUserExist(String name, String password);
-
-    UserEntity findRoleByUser(String name, String password);
 
 }
