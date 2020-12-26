840934b2229c112337bd83d4c80fc4e2ab61fd5f
Nguyen Linh Phuong <nlphuong95@gmail.com>
2020-12-26 13:35:02 +0700

    Ticket #13 command partern and init logger

--- toeic-web-logic/src/main/java/com/pnguyen/command/UserCommand.java
+++ toeic-web-logic/src/main/java/com/pnguyen/command/UserCommand.java
@@ -3,9 +3,25 @@ package com.pnguyen.command;
 import com.pnguyen.core.dto.UserDTO;
 import com.pnguyen.core.web.command.AbstractCommand;
 
+/*
+    Logic model:
+        command -> pojo -> DTO
+
+*/
+
 public class UserCommand extends AbstractCommand<UserDTO> {
+    private String confirmPassword;
+
     public UserCommand(){
         this.pojo = new UserDTO();
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
 
 }
--- toeic-web-logic/src/main/java/com/pnguyen/controllers/admin/LoginController.java
+++ toeic-web-logic/src/main/java/com/pnguyen/controllers/admin/LoginController.java
@@ -1,5 +1,10 @@
 package com.pnguyen.controllers.admin;
 
+import com.pnguyen.command.UserCommand;
+import com.pnguyen.core.dto.UserDTO;
+import com.pnguyen.core.web.utils.FormUtil;
+import org.apache.log4j.Logger;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +15,7 @@ import java.io.IOException;
 
 @WebServlet("/login.html")
 public class LoginController extends HttpServlet {
+    private final Logger logger = Logger.getLogger(this.getClass());
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher rd = req.getRequestDispatcher("/views/web/login.jsp");
@@ -18,6 +24,9 @@ public class LoginController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        /*UserCommand command = FormUtil.populate(UserCommand.class, req);
+        UserDTO pojo = command.getPojo();*/
+        RequestDispatcher rd = req.getRequestDispatcher("/views/web/login.jsp");
+        rd.forward(req, resp);
     }
 }
